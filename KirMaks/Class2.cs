e472f13928078c883579b9c5a868d1649d66755a
Student <Student@EMIS.local>
2018-02-24 19:26:25 +0700

    REC

---
+++ KirMaks/Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KirMaks
+{
+    class Class2 : Class1
+    {
+        public override void Draw(PictureBox cc)
+        {
+            Graphics g = cc.CreateGraphics();
+            g.Clear(Color.WhiteSmoke);
+            g.DrawRectangle(new Pen(Color.Red), 300, 80, 80, 80);
+        }
+    }
+}
